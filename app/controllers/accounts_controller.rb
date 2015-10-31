84666a42b1367cc1d4935863df94cdca032992ba
kangkyu <kangkyu1111@gmail.com>
2015-10-30 23:22:59 -0700

    run server for accounts index and react-rails setup working

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,11 +7,11 @@ class AccountsController < ApplicationController
     @accounts = Account.all
 
     respond_to do |format|
-      format.html { @accounts }
+      format.html { render component: 'AccountList', props: { accounts: @accounts }, tag: 'span' }
       format.json { render json: @accounts }
     end
     # This custom renderer behaves the same as a normal view renderer and accepts the usual arguments - content_type, layout, location and status. By default, your current layout will be used and the component, rather than a view, will be rendered in place of yield.
-    render component: 'AccountList', props: { accounts: @accounts }, tag: 'span'
+    
   end
 
   # GET /accounts/1
